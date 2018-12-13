f06b267788a312e5cb7a324dcb42e902d6c69d6a
azzaakamoun <azza.kammoun@unil.ch>
2018-12-13 16:45:36 +0100

    added rule of the game

--- project/app.R
+++ project/app.R
@@ -8,21 +8,151 @@ library(shinyjs)
 ui <- fluidPage( includeCSS("www/bootstrap.css"), 
                  navbarPage(h1("THE MOJITO GAME",
                                style = "font-family: 'Neucha', cursive;
-                               font-weight: 500; line-height: 1.1; font-size : 60px;
+                               font-weight: 500; line-height: 1.1;
                                color: #28a745;"),
                             
                             tabPanel(
                               h1("Rules of the game", 
                                  style = "font-family: 'Neucha', cursive;
-                                 font-weight: 500; line-height: 1.1; 
+                                        font-weight: 500; line-height: 1.1; 
                                  color: #28a745;"),
-                              tags$body("Welcome to our game ! 
-                                        Here you will be the manager of a Mojito Stand.
-
-                                        note that you can only go to negative 200$ meaning you cannot loan more money
-                                        i also changed marketing to 100$ dollar instead on 250$ 
-                                        also each 10 seconds you lose stock ammount*0.1$ and 1$ per server
-                                        ")),
+                             
+                               tags$body(h2("Welcome to our game !",
+                                           style = "font-family: 'Neucha', cursive;
+                                           font-weight: 500; padding-top: 20px;
+                                            padding-bottom: 20px;
+                                           line-height: 1.1; font-size : 40px;
+                                           margin-top:30px; text-align: center"),
+                                        h3 (em("What is Mojito bro?"),
+                                            style = "font-family: 'Neucha', cursive;
+                                            line-height: 1.1; font-size : 30px; 
+                                            padding-bottom: 20px"),
+                                        p("Here you will be the MANAGER of a Mojito Stand",
+                                          style = "line-height: 1.1; font-size : 18px"),
+                                        br(),
+                                        h3 (em("What do you need to do?"),
+                                            style = "font-family: 'Neucha', cursive;
+                                            line-height: 1.1; font-size : 30px; 
+                                            padding-bottom: 20px;"),
+                                        p("Cost Parameter",
+                                          style = "line-height: 1.1; font-size : 18px;
+                                          color:  #17a2b8"),
+                                          tags$ol (tags$li("You will choose a marketing level",
+                                            tags$img(src = "https://png.pngtree.com/element_our/md/20180304/md_5a9bfb1dc16b8.png",
+                                                    width = "30px", height = "30px"), 
+                                            tags$img(src = "https://i0.wp.com/www.waldo.be/wp-content/uploads/2014/04/warning-sign1.jpg",
+                                                   width = "20px", height = "20px"),
+                                        "Be careful once you increase it you cannot decrease it
+                                        Any increase will lead to a cost of 100 $"),
+                                          tags$li("You have to choose the number", 
+                                                "of servers",
+                                                tags$img(src = "https://png.pngtree.com/element_our/md/20180304/md_5a9bfb1dc16b8.png",
+                                                         width = "30px", height = "30px"),
+                                                "each server costs 1$ by 10 seconds"),
+                                          tags$li("Don’t worry you will have a box indicating how much are your costs!!"),
+                                        style = "line-height: 2; font-size : 18px;
+                                        margin-left: 5%"
+                                        ),
+                                        p("Supply Parameter",
+                                          style = "line-height: 1.1; font-size : 18px;
+                                          color:  #17a2b8"),
+                                        p("To produce Mojitos you need raw materials.",
+                                          "For that you had to negotiate with many suppliers",
+                                          "and you could finally get these agreements",
+                                          "for each ingredient:",
+                                          style = "line-height: 2; 
+                                          font-size : 18px; margin-left: 4%"),
+                                        tags$ol(
+                                          tags$li("Lemon",
+                                                  tags$img(src = "https://www.dictionary.com/e/wp-content/uploads/2018/09/lemon.png",
+                                                           width = "20px", height = "20px"),
+                                                  ": Your supplier can give",
+                                          " you at least a 10 lemon stock for 5$"),
+                                          tags$li("Sugar",
+                                                  tags$img(src = "http://eboxmart.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/s/u/sugar_s_1_kg_1100_1.jpg",
+                                                           width = "20px", height = "20px"),
+                                                  ": Your supplier can  sell to ",
+                                                  "you at least a 30 sugar stock for 5$"),
+                                          tags$li("Mint",
+                                                  tags$img(src = "http://icons.iconarchive.com/icons/robinweatherall/chocolate/256/mint-leaf-icon.png",
+                                                           width = "20px", height = "20px"),
+                                                  ": you can buy at least",
+                                                  "a 20 mint stock for 5$"),
+                                          tags$li("Rhum ",
+                                                  tags$img(src = "https://privatewhiskysociety.com/wp-content/uploads/2018/01/Rhum-Cadenhead-Boutique.jpg",
+                                                           width = "25px", height = "25px"),
+                                                  ": you can buy at least",
+                                                  "a 10 Rhum stock for 5$"),
+                                          style = "line-height: 2; 
+                                          font-size : 18px; margin-left: 5%"
+                                          ),
+                                        p("Again you will have a box indicating",
+                                          " how many mojitos you can produce with",
+                                          " your available stocks",
+                                          tags$img(src = "https://i.pinimg.com/originals/ea/d9/86/ead9860697d01fc09c7c7d8c3dc61dee.jpg",
+                                                   width = "20px", height = "20px"),
+                                          style = "line-height: 2; 
+                                          font-size : 18px; margin-left: 5%"),
+                                        p("Price and ambiance set up",
+                                          style = "line-height: 1.1; font-size : 18px;
+                                          color:  #17a2b8"),
+                                        tags$ol(
+                                          tags$li("You have to fix your selling price:",
+                                                  "Remember your pricing strategy classes",
+                                                  tags$img(src = "https://i1.wp.com/www.papertraildesign.com/wp-content/uploads/2017/06/emoji-nerd-glasses.png",
+                                                           width = "20px", height = "20px"),
+                                                  style = "line-height: 2; font-size : 18px"),
+                                          tags$li("Select your ambiance:",
+                                                  "Your challenge is to find which ambiance your client prefer"),
+                                          style = "line-height: 2; 
+                                          font-size : 18px; margin-left: 5%"
+                                          ),
+                                        h3 (em("When do you win?"),
+                                            style = "font-family: 'Neucha', cursive;
+                                            line-height: 1.1; font-size : 30px; 
+                                            padding-bottom: 20px"),
+                                        p("When you achieve a profit of 1000$",
+                                          tags$img(src = "https://dejpknyizje2n.cloudfront.net/svgcustom/clipart/preview/6a24a9edae1246739dd6b8c06293bc18.png",
+                                                   width = "30px", height = "30px"),
+                                          style = "line-height: 2; font-size : 18px;
+                                          margin-left: 5%"),
+                                        p("Gross profit is not your piggy bank",
+                                          tags$img(src = "https://i0.wp.com/www.waldo.be/wp-content/uploads/2014/04/warning-sign1.jpg",
+                                                   width = "30px", height = "30px"),
+                                          style = "line-height: 2; font-size : 18px;
+                                          margin-left: 5%"),
+                                        p("Remember your accounting classes",
+                                          tags$img(src = "https://i1.wp.com/www.papertraildesign.com/wp-content/uploads/2017/06/emoji-nerd-glasses.png",
+                                                   width = "25px", height = "25px"),
+                                          style = "line-height: 2; font-size : 18px;
+                                          margin-left: 5%"),
+                                        h3 (em("When do you lose?"),
+                                            style = "font-family: 'Neucha', cursive;
+                                            line-height: 1.1; font-size : 30px; 
+                                            padding-bottom: 20px"),
+                                        p("Your piggy bank can go to negative 200$",
+                                          tags$img(src = "https://www.ahundredmonkeys.com/wp-content/uploads/2015/03/EMOmoney-300x260.jpeg",
+                                                   width = "30px", height = "30px"),
+                                          br(),
+                                          tags$img(src = "https://png.pngtree.com/element_our/md/20180304/md_5a9bfb1dc16b8.png",
+                                                   width = "30px", height = "30px"),
+                                          "Which means that you cannot have a loan",
+                                          " higher than 200 and you will have to",
+                                          " announce bankruptcy",
+                                          style = "line-height: 2; font-size : 18px;
+                                          margin-left: 5%",
+                                          br(),
+                                          "Or you have less than 5$ in your bank ",
+                                          "and you cannot make any more mojitos"
+                                          ),
+                                        h2("Good Luck !",
+                                           br(),
+                                           style = "font-family: 'Neucha', cursive;
+                                           font-weight: 500; padding-top: 20px;
+                                           padding-bottom: 20px;
+                                           line-height: 1.1; font-size : 40px;
+                                           margin-top:30px; text-align: center")
+                                        )),
                             
                             tabPanel(h1("The Game", 
                                         style = "font-family: 'Neucha', cursive;
